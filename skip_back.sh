7dd173de6148489d89396cde0a37f399e3d64b4c
root <anthonyfleet@hotmail.com>
2019-09-06 23:59:42 +0100

    New files

---
+++ skip_back.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+curl -X "POST" "https://api.spotify.com/v1/me/player/previous" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQDPFa_p3C-dc4wccbHsIwh2uOhZgIR-9eubRyTSX8m-0SwLyYuw1odQT0Wos7ElUAg5a23GQB9__MMYCl0W794gWLh7N9M7CIwmJzcFAMYqv-qjFh_hgZk5Vod4JuedNVlK7IfLoFnnNiFNZg" -H "Content-length: 0"
